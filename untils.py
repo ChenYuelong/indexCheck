68954a2b58e446f735db34af493132571f39d896
chenyl <yuelong_chen@yahoo.com>
2018-01-18 13:56:10 +0800

    Initial commit

---
+++ untils.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# @Time    : 2018/1/18 下午1:54
+# @Author  : chenyuelong
+# @Mail    : yuelong_chen@yahoo.com
+# @File    : untils.py
+# @Software: PyCharm
+
+import os, sys
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
